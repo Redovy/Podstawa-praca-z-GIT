18b31805e1c6deb684702e02ff2124677d18dc8e
Redovy <dsw49450@student.dsw.edu.pl>
2023-04-25 20:42:46 +0200

    Tworzenie logów w ilości podanej przez użytkownika

--- skrypt.sh
+++ skrypt.sh
@@ -7,11 +7,22 @@ while [ $# -gt 0 ]; do
             exit 0
             ;;
         --logs)
-            for i in {1..100}; do
-                echo "Nazwa pliku: log$1.txt" > log$i.txt
-                echo "Komenda wywołania skryptu: $0" >> log$i.txt
-                echo "Data utworzenia pliku: $(date)" >> log$i.txt
-            done
+            shift
+            if [[ $1 =~ ^-?[0-9]+$ ]]; then
+                n=${1#}
+                for i in $(seq 1 $n); do
+                    echo "Nazwa pliku: log$i.txt" > log$i.txt
+                    echo "Komenda wywołania skryptu: $0 $1" >> log$i.txt
+                    echo "Data utworzenia pliku: $(date)" >> log$i.txt
+                done
+            else
+                for i in {1..100}; do
+                  echo "Nazwa pliku: log$i.txt" > log$i.txt
+                  echo "Komenda wywołania skryptu: $0 $1" >> log$i.txt
+                  echo "Data utworzenia pliku: $(date)" >> log$i.txt
+                done
+                exit 1
+            fi
             exit 0
             ;;
     esac
