59939e329e41bdacbd4abd6dc848af6387282cd6
Redovy <dsw49450@student.dsw.edu.pl>
2023-04-25 20:30:02 +0200

    Automatyczne tworzenie logów

--- skrypt.sh
+++ skrypt.sh
@@ -6,6 +6,14 @@ while [ $# -gt 0 ]; do
             date
             exit 0
             ;;
+        --logs)
+            for i in {1..100}; do
+                echo "Nazwa pliku: log$1.txt" > log$i.txt
+                echo "Komenda wywołania skryptu: $0" >> log$i.txt
+                echo "Data utworzenia pliku: $(date)" >> log$i.txt
+            done
+            exit 0
+            ;;
     esac
     shift
 done
